c1cf22ad66795c83f30980d587fb597b2185e611
Kesdy10 <kesdy.01@gmail.com>
2025-10-10 19:49:54 -0600

    commit
    aaa

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -62,4 +63,5 @@ def nikegato():
     return render_template('productosNike/nikegato.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)

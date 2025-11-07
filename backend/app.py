d5c5bc2b2e8567a6c80dfe36552a249d32b0d7b8
Kesdy10 <kesdy.01@gmail.com>
2025-11-07 15:13:20 -0600

    commit

--- backend/app.py
+++ backend/app.py
@@ -696,5 +696,8 @@ def reebokquestion():
     return render_template('productosReebok/reebokquestion.html', producto=producto)
 
 if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
+if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
